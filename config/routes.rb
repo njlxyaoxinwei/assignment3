3ff1c22b422ec29320e2b2c41822a2692a8196ff
David Yao <njlxyaoxinwei@gmail.com>
2013-03-20 16:29:36 -0400

    Added StaticPages Controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 RedditDemo::Application.routes.draw do
+  match '/home', to: 'static_pages#home'
+
+  match '/about', to: 'static_pages#about'
+
+  match '/contact', to: 'static_pages#contact'
+
   resources :users
   root to: 'users#index'
 end
